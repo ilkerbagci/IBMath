6bcbd8214ab4b8a1130f8b46f3ec7b516ba72a83
İlker Bağcı <ilkerrbagci@gmail.com>
2020-01-21 13:16:36 +0300

    Initial Project Setup

---
+++ IBMath/IBMath.h
@@ -0,0 +1,19 @@
+//
+//  IBMath.h
+//  IBMath
+//
+//  Created by İlker Bağcı on 21.01.2020.
+//  Copyright © 2020 İlker Bağcı. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IBMath.
+FOUNDATION_EXPORT double IBMathVersionNumber;
+
+//! Project version string for IBMath.
+FOUNDATION_EXPORT const unsigned char IBMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBMath/PublicHeader.h>
+
+
---
+++ IBMathExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBMathExample
+//
+//  Created by İlker Bağcı on 21.01.2020.
+//  Copyright © 2020 İlker Bağcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
